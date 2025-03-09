30493a833796e13b3770f4fd51c85795933557e3
qingzhixing <qzsqqmail@qq.com>
2025-03-09 18:23:25 +0800

    统一rip获取

--- src/kernel/trap.c
+++ src/kernel/trap.c
@@ -10,72 +10,72 @@ void debug_print_stack(unsigned long rsp) {
 }
 
 void do_divide_error(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_divide_error(0),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_debug(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_debug(1),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp, rip);
 	while (1);
 }
 
 void do_nmi(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_nmi(2),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp, rip);
 	while (1);
 }
 
 void do_int3(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_int3(3),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp, rip);
 	while (1);
 }
 
 void do_overflow(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_overflow(4),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp, rip);
 	while (1);
 }
 
 void do_bounds(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_bounds(5),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp, rip);
 	while (1);
 }
 
 void do_undefined_opcode(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_undefined_opcode(6),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_dev_not_available(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_dev_not_available(7),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_double_fault(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_double_fault(8),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_coprocessor_segment_overrun(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_coprocessor_segment_overrun(9),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n",
 				 error_code, rsp, rip);
 	while (1);
 }
 
 void do_invalid_TSS(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_invalid_TSS(10),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 
@@ -100,7 +100,7 @@ void do_invalid_TSS(unsigned long rsp, unsigned long error_code) {
 }
 
 void do_segment_not_present(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_segment_not_present(11),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 
@@ -125,7 +125,7 @@ void do_segment_not_present(unsigned long rsp, unsigned long error_code) {
 }
 
 void do_stack_segment_fault(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_stack_segment_fault(12),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 
@@ -150,7 +150,7 @@ void do_stack_segment_fault(unsigned long rsp, unsigned long error_code) {
 }
 
 void do_general_protection(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_general_protection(13),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 
@@ -216,35 +216,35 @@ void do_page_fault(unsigned long rsp, unsigned long error_code) {
 }
 
 void do_x87_FPU_error(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_x87_FPU_error(16),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_alignment_check(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_alignment_check(17),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_machine_check(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_machine_check(18),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_SIMD_exception(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_SIMD_exception(19),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code, rsp,
 				 rip);
 	while (1);
 }
 
 void do_virtualization_exception(unsigned long rsp, unsigned long error_code) {
-	unsigned long rip = (struct pt_regs *)->rip;
+	unsigned long rip = ((struct pt_regs *) rsp)->rip;
 	color_printk(RED, BLACK, "do_virtualization_exception(20),ERROR_CODE:%#018lx,RSP:%#018lx,RIP:%#018lx\n", error_code,
 				 rsp, rip);
 	while (1);
